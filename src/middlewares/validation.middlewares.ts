5dab668abe09e6a4e80b396c9df1f2c5ee0cc8f6
Shiroo <hmegxva@gmail.com>
2025-07-02 16:56:01 +0700

    fix: 401 for access token

--- src/middlewares/validation.middlewares.ts
+++ src/middlewares/validation.middlewares.ts
@@ -1,16 +1,21 @@
 import { ValidationChain, validationResult } from 'express-validator'
 import { NextFunction, Request, Response } from 'express'
-import { BadRequestError } from '~/core/error.response'
+import { AuthRequestError, BadRequestError } from '~/core/error.response'
+import { AuthorizationError } from 'passport-oauth2'
 
 export const validate = (validate: ValidationChain[]) => {
   return async (req: Request, res: Response, next: NextFunction) => {
     try {
       await Promise.all(validate.map((validation) => validation.run(req)))
       const errors = validationResult(req)
-      console.log(errors)
 
       if (!errors.isEmpty()) {
         const errorMessages = errors.array().map((error) => error.msg)
+
+        if (errors.array()[0].location == 'headers') {
+          return next(new AuthRequestError(errorMessages[0]))
+        }
+
         next(
           new BadRequestError({
             message: errorMessages[0]
